cac4714fd8f1852f2290b145ecab3fb79b4a7f8b
kei1115 <keifukuda1115@gmail.com>
2021-11-27 19:28:39 +0900

    Add ようこそ

--- content_script.js
+++ content_script.js
@@ -1,5 +1,9 @@
 function f(n){
     let cs = n.childNodes;
+    let timeline = document.getElementsByClassName("usertext mr-1")[0];
+    if (timeline.textContent == "学生 福田　恵一朗") {
+      timeline.textContent = timeline.textContent + "さん ようこそ！";
+    }
     for(let i = 0; i < cs.length; i++){
       let c = cs[i];
       if(c.nodeType == Node.TEXT_NODE){
