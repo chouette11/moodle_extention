9f44b4a7795c441cbbed3fbf3a2fca11ea11a0c4
kei1115 <keifukuda1115@gmail.com>
2022-03-29 18:21:24 +0900

    Big commit

--- alert.js
+++ alert.js
@@ -1,15 +1,53 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwz1kET7wZc4uz420f6tg__xftWMXI9H8",
+  authDomain: "task-changer.firebaseapp.com",
+  projectId: "task-changer",
+  storageBucket: "task-changer.appspot.com",
+  messagingSenderId: "1004421656533",
+  appId: "1:1004421656533:web:7c66c2bdcbea64b3ae575c",
+  measurementId: "${config.measurementId}"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import { collection, addDoc } from "firebase/firestore";
+
+function addData() {
+    try {
+    const docRef = await addDoc(collection(db, "users"), {
+        first: "Adaaaaaaa",
+        last: "Lovelace",
+        born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
+}
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+
 function showAlert() {
     console.log("どうなってんだ");
 	alert("今日も楽しく開発しよう！");
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({
-        url: 'sample.html'
-    });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: showAlert
-	});
-    console.log(doc);
-});
+document.getElementById("alertButton").onclick = function() {
+	addData();
+    alert("今日も楽しく開発しよう！");
+};
--- content_script.js
+++ content_script.js
@@ -1,32 +1,74 @@
 function f(n){
-    let cs = n.childNodes;
-    let timeline = document.getElementsByClassName("usertext mr-1")[0];
-    if (timeline.textContent == "学生 福田　恵一朗") {
-      timeline.textContent = timeline.textContent + "さん ようこそ！";
-    }
-    var aria_label = "";
+    const date = new Date();
+    const Y = date.getFullYear();
+    const M = date.getMonth();
+    const D = date.getDate();
+    const H = date.getHours();
+    const m = date.getMinutes();
+
     setTimeout(() => {
-
-      aria_label = document.getElementsByClassName('w-100 event-name-container text-truncate line-height-3')[0].getElementsByTagName("a")[0].ariaLabel;
-      
-      var limit = "";
-      for (var i = aria_label.indexOf("活動は") + 4; i < aria_label.indexOf("活動は") + 23; i++) {
-        limit += aria_label[i];
+      var count = 0;
+      const labelList = document.getElementsByClassName('w-100 event-name-container text-truncate line-height-3');
+        for (var k = 0; k < labelList.length; k++) {
+        const aria_label = document.getElementsByClassName('w-100 event-name-container text-truncate line-height-3')[k].getElementsByTagName("a")[0].ariaLabel;
+        
+        var limit = "";
+        for (var i = aria_label.indexOf("活動は") + 4; i < aria_label.indexOf("活動は") + 23; i++) {
+          limit += aria_label[i];
+        }
+        const year = limit[0] + limit[1] + limit[2] + limit[3];
+        const month = limit[6] + limit[7];
+        const day = limit[10] + limit[11];
+        const hour = limit[14] + limit[15];
+        const minute = limit[17] + limit[18];
+        var remaining = "残り";
+        var days = (parseInt(year) - Y) * 365 + (parseInt(month) - M - 1) * 30 + (parseInt(day) - D);
+        var hours = (parseInt(hour) - H);
+        var minutes = (parseInt(minute) - m);
+        if (minutes < 0) {
+          hours--;
+          minutes += 60;
+        }
+        if (hours < 0) {
+          days--;
+          hours += 24;
+        }
+        if (days > 0) {
+          remaining += `${days}日`;
+        }
+        if (hours > 0) {
+          remaining += `${hours}時間`;
+        }
+        if (minutes > 0) {
+          remaining += `${minutes}分`;
+        }
+        if (days < 0 || hours < 0 || minutes < 0) {
+          count++;
+          var positive = ["あなたは頑張っている", "ドンマイ！", "次頑張ろう！", "まだ大丈夫！", "大変だったんだね", "たまには息抜きしてね"]
+          remaining = positive[minutes * 100 % (k + 1)];
+        }
+        var def_limit = document.getElementsByClassName("text-right text-nowrap ml-1")[k];
+        def_limit.textContent = remaining;
+      }
+      if (count > 8) {
+        for (var k = 0; k < count; k++) {
+          var def_limit = document.getElementsByClassName("text-right text-nowrap ml-1")[k];
+          def_limit.textContent = "甘えるな";
+        }
       }
 
-      var def_limit = document.getElementsByClassName("text-right text-nowrap ml-1")[0];
-      def_limit.textContent = limit;
+       // 新しい div 要素を作成します
+      var newDiv = document.createElement("div");
+      // いくつかの内容を与えます
+      var newContent = document.createTextNode("なんかゴリ押したらいけました");
+      // テキストノードを新規作成した div に追加します
+      newDiv.appendChild(newContent);
+
+      // DOM に新しく作られた要素とその内容を追加します
+      var parrent = document.getElementById("page-content");
+      var child = document.getElementById("region-main-box")
+      parrent.insertBefore(newDiv, child);
     }, 4000);
-    
-    for(let i = 0; i < cs.length; i++){
-      let c = cs[i];
-      if(c.nodeType == Node.TEXT_NODE){
-        c.textContent = c.textContent.replace("有機", "でござるよ。");
-      }
-      else{
-        f(c);
-      }
-    }
   }
   
   f(document.body);
