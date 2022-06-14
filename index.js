08383215a3bd1fc6d5394a402912bb654fe72a94
chouette11 <keifukuda1115@gmail.com>
2022-06-14 20:36:41 +0900

    add: Add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("view_today").innerHTML = getToday(number());
+
+function getToday(n) {
+	for (var i = 0; i < n; i++) {
+        var newDiv = document.createElement("button");
+    }
+	return s;
+}
+
+function number() {
+    // 課題の数を取得
+    return 3;
+}
