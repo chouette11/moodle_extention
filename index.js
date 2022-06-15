ab46ed48f644aed1db9e0738849dd12592406721
Kingoara <saku.itozono@icloud.com>
2022-06-15 17:30:35 +0900

    add_button

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-document.getElementById("view_today").innerHTML = getToday(number());
+    //div生成
+    //creatElementでJS内でhtmlのDOMを生成できる
+    const div = document.createElement("div");
+    //classNameでdivのクラスを付与できる
+    div.className = "list-row";
+  
+    for(var i=0;i<number();i++){
+    //button()タグ生成
+    const button = document.createElement("button");
+    
+    div.appendChild(button);
 
-function getToday(n) {
-	for (var i = 0; i < n; i++) {
-        var newDiv = document.createElement("button");
     }
-	return s;
-}
+    document.getElementById("view_today").appendChild(div);
+
 
 function number() {
     // 課題の数を取得
