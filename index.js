5a92568a8b8ccc0924b53d0506467ba6c2c731f5
ryuryu5121 <r.oshima.12.15@gmail.com>
2022-06-15 17:40:28 +0900

    Make component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.getElementById("view_today").innerHTML = getToday(number());
+
+document.getElementById("here").append(makeComponent("第4回課題","残り4時間","有機化学機器"));
 
 function getToday(n) {
 	for (var i = 0; i < n; i++) {
@@ -10,4 +11,37 @@ function getToday(n) {
 function number() {
     // 課題の数を取得
     return 3;
+}
+
+
+function makeComponent(title, limit, lectureName) {
+    let container = document.createElement("div");
+
+    // let header = document.createElement("div");
+    // let titleArea = document.createElement("div");
+    // let limitArea = document.createElement("div");
+    // let lectureArea = document.createElement("div");
+
+    // titleArea.innerHTML = title;
+    // limitArea.innerHTML = limit;
+    // lectureArea.innerHTML = lectureName;
+    // header.append(titleArea,limitArea);
+    // container.append(header,lectureArea);
+
+    // header.classList.add("d-flex","justify-content-between","red");
+    // container.classList.add("column","gray");
+    container.innerHTML = "";
+    container.innerHTML += `
+    <div class="column gray">
+        <div class="justify-content-between d-flex">
+            <div>${title}</div>
+            <div>${limit}</div>
+        </div>
+        <div class="justify-content">
+        ${lectureName}
+        </div>
+    </div>
+    `;
+
+    return container;
 }
